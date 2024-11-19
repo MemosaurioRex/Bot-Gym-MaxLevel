311df0526d817eaa3a35703557ac5689f349685b
MemosaurioRex <40439763+MemosaurioRex@users.noreply.github.com>
2024-11-19 15:21:25 -0300

    Bot conectado a la base de datos. - Ya lee correctamente las clases.

--- src/intents/ScheduledHours/scheduledHours.intent.js
+++ src/intents/ScheduledHours/scheduledHours.intent.js
@@ -1,4 +1,8 @@
 import { WSP_MSG_LIST } from "../../api/axios";
+import ClassesModel from "../../models/Classes";
+import TypeClassesModel from "../../models/TypeClasses";
+import moment from "moment-timezone"
+moment.locale('es')
 
 export const shceduledHours = async ( credentials ) => {
 
@@ -7,30 +11,22 @@ export const shceduledHours = async ( credentials ) => {
    * El filtro debe ver cuantas clases tiene el usuario, si pasan X cantidad
    * el usuario no puede agendar mas clases.
    */
+  const data = await ClassesModel.find({}).populate("typeClass").limit(10);
 
   const header_data = {
     titulo: "Clases", cuerpo: "Con el botón _Tomar clases_ podrás tomar clases que estén libres",
-    footer: "{{name_org}}", button: "Tomar clases"
+    footer: process.env.NOMBRE_ORG, button: "Tomar clases"
   };
 
   const list_data = [];
-
-  list_data.push({
-    id: "Clases de Spinning",
-    title: "Spinning",
-    description: "Todos los días a las 08:00 - 09:00\n"+"Clases intensivas\n"
-  },{
-    id: "Clases de Levantamiento olímpico",
-    title: "Levantamiento olímpico",
-    description: "Todos los días a las 15:00 - 17:00\n"+"Clases básicas\n"
-  },{
-    id: "Clases de Calistenia",
-    title: "Calistenia",
-    description: "Todos los días a las 09:00 - 10:00\n"+"Clases básicas\n"
-  },{
-    id: "Clases de Yoga",
-    title: "Yoga",
-    description: "Todos los días a las 19:00 - 21:00\n"+"Clases básicas\n"
+  var count = 0;
+  data.forEach(element => {
+    list_data.push({
+      id: `${element.typeClass.name} N°${count}`,
+      title: element.typeClass.name,
+      description: `${moment(element.date).format('LL')} Desde las ${element.start} hasta las ${element.end}`
+    });
+    count++;
   });
   
   WSP_MSG_LIST (credentials.phone_number_id, credentials.from, header_data, list_data);
